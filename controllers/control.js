800a02b599ef24f3e49be4742676c1c1874693e9
pache <pachecodossantos17@gmail.com>
2022-11-30 14:42:09 -0300

    Adiciona o Insert do CRUD

--- controllers/control.js
+++ controllers/control.js
@@ -9,3 +9,17 @@ export const getT = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+export const addInfo = (req, res) => {
+    const nome = req.body.nome;
+    const idade = req.body.idade;
+    const apelido = req.body.apelido;
+
+    const sql = `INSERT INTO cadastro(nome, idade, apelido) VALUES ('${nome}', '${idade}', '${apelido}')`;
+
+    db.query(sql, (err) => {
+        if (err) return res.json(err);
+
+        res.status(200).json('Enviado com sucesso');
+    });
+};
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getT } from '../controllers/control.js';
+import { addInfo, getT } from '../controllers/control.js';
 
 
 const route = express.Router();
 route.get('/', getT);
+route.post('/add', addInfo);
 
 export default route;
